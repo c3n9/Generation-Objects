ebb798effa4743300df84a9e599749563f6c408f
c3n9 <arseniy11041104@gmail.com>
2023-06-11 16:14:25 +0300

    generate table

---
+++ GenerateTable/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using GenerateTable.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GenerateTable
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static OrderEntities DB = new OrderEntities();
+    }
+}
---
+++ GenerateTable/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GenerateTable
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ListView listView = new ListView();
+            GridView gridView = new GridView();
+            gridView.Columns.Add(new GridViewColumn { Header = "Id" , DisplayMemberBinding=new Binding("Id")});
+            gridView.Columns.Add(new GridViewColumn { Header = "Номер заказа" , DisplayMemberBinding = new Binding("OrderNumber") });
+            gridView.Columns.Add(new GridViewColumn { Header = "Дата заказа" , DisplayMemberBinding = new Binding("Date") });
+            gridView.Columns.Add(new GridViewColumn { Header = "Продукт", DisplayMemberBinding = new Binding("Product.Name") });
+            listView.View = gridView;
+            MainGrid.Children.Add(listView);
+            listView.ItemsSource = App.DB.Order.ToList();
+
+        }
+    }
+}
